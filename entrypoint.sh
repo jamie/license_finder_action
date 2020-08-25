a812a039c412b38960b971a8f44e645e97b45b7f
Jamie Macey <jamie.git@tracefunc.com>
2020-08-24 21:57:25 -0700

    handle error report inside error trap

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,30 +23,20 @@ if [ "$?" -ne 0 ]; then
 	exit 1
 fi
 
-#
-# Run the tool
-#
-# This inspects all the dependencies, and determines their version and licenses
-# JSON output is used for further steps to consume
-#
+
 REPORT=$(license_finder --format json)
 if [ "$?" -ne 0 ]; then
 	echo "Could not run the report tool!"
 	echo "$REPORT"
+
+	# Minimize JSON
+	# Remove all output until the start of the JSON (I.E. "{" char)
+	# Base64 is used to ensure we don't have issues with special characters
+	# Condense to a single line
+	REPORT=$(echo $REPORT | sed 's/[^{]*{/{/' | base64 | tr -d '\n')
+
+	# Special output for Github Actions to read the value
+	echo "##[set-output name=license_report_json;]$REPORT"
+
 	exit 1
 fi
-echo "Report: $REPORT"
-
-#
-# Minimize JSON
-#
-# Remove all output until the start of the JSON (I.E. "{" char)
-#    license_finder can be chatty
-# Base64 is used to ensure we don't have issues with special characters
-# Condense to a single line
-#
-REPORT=$(echo $REPORT | sed 's/[^{]*{/{/' | base64 | tr -d '\n')
-
-# Special output for Github Actions to read the value
-echo "##[set-output name=license_report_json;]$REPORT"
-
